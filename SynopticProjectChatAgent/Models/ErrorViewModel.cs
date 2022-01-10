69b6e4f131236e5e117da83488d72d9133a7ac8b
Charlie Amos <Charlie.Amos@jet2.com>
2022-01-10 10:25:38 +0000

    Add project files.

---
+++ SynopticProjectChatAgent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SynopticProjectChatAgent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SynopticProjectChatAgent/Models/Holiday.cs
@@ -0,0 +1,25 @@
+﻿namespace SynopticProjectChatAgent.Models
+{
+    public class Holiday
+    {
+        public int HoliidayReference { get; set; }
+
+        public string  HotelName { get; set; }
+
+        public string City { get; set; }
+
+        public string Continent { get; set; }
+
+        public string Country { get; set; }
+
+        public string Category { get; set; }
+
+        public int StarRating { get; set; }
+
+        public string TempRating { get; set; }
+
+        public string Location { get; set; }
+
+        public decimal PricePerNight { get; set; }
+    }
+}
