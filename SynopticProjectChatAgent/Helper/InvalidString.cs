98a5d891d1824d93bac5319dcf210374485e4575
Charlie Amos <Charlie.Amos@jet2.com>
2022-01-12 12:53:51 +0000

    DBDeployment

---
+++ SynopticProjectChatAgent/Helper/InvalidString.cs
@@ -0,0 +1,7 @@
+﻿namespace SynopticProjectChatAgent.Helper
+{
+    public class InvalidString
+    {
+        public string MyProperty { get; set; }
+    }
+}
--- SynopticProjectChatAgent/Tests/StoredProcedureTest.cs
+++ SynopticProjectChatAgent/Tests/StoredProcedureTest.cs
@@ -7,31 +7,26 @@ namespace SynopticProjectChatAgent.Tests
     public class StoredProcedureTest
     {
         [Test]
-        private void Stored_Procedure_is_Called()
+        public void Stored_Procedure_is_Called()
         {
             DataConnection connection = new DataConnection();
             //arrange
-             string hotelName= "TechCity";
 
              string tempRating = "mild";
 
              string continent= "Asia";
 
-             string country="Japan";
 
              string category="active";
 
-             int starRating= 3;
 
              string location="city";
-
-             decimal pricePerNight=71; 
 
         //expected
         List<Holiday> expectedResults = new List<Holiday>()
             {
-                new Holiday{ Category = category,HotelName= hotelName,TempRating=tempRating,Continent=continent,Country=country,
-                    StarRating=starRating,Location=location,PricePerNight=pricePerNight }
+                new Holiday{ Category = category,TempRating=tempRating,Continent=continent,
+                 Location=location}
             };
             //act
             var actualResult = connection.GetFilteredHolidays(expectedResults,continent,category,location,tempRating);
